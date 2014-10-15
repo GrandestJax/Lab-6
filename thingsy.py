bcac1b676a8d02eb6d18922e0cad7e0e9a8c4759
GrandestJax <491659@student.roundrockisd.org>
2014-10-15 15:11:03 -0500

    first

---
+++ thingsy.py
@@ -0,0 +1,27 @@
+# Partner lab 8 example
+
+import random
+
+print "A group of monsters approach! Prepare to fight!"
+
+playerHealth = 100
+icemonsterHealth = 100
+monsterHealth = 100
+punchDmg = 5
+swordDmg = 10
+fireballDmg = 30
+
+# TODO:
+# Loop - When should it stop?
+# Input - user gets to choose attack
+# Calculating the damage done by the user and the monster
+# Output - printing damage
+# Printing a victory message
+
+
+import random
+damageByMonster = random.randint(1,35)
+playerHealth = playerHealth - damageByMonster 
+import random
+damageByiceMonster = random.randint(1,45)
+playerHealth = playerHealth - damageByiceMonster 
